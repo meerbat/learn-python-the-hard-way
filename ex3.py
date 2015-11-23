f4998692c6483249b8c67b61e5d74e994619b7e8
miriam battistoni <miriam@giorgio.ufficio>
2015-11-23 16:36:10 +0100

    Esercizio 3 con calcoli di base su interi

---
+++ ex3.py
@@ -0,0 +1,12 @@
+print "I will now couunt:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 *3 % 4
+print "NOw i will count my eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2  -1 / 4 + 6
+print "true that 3+2= 5-7?"
+print 3 + 2  < 5 - 7
+print "3+2", 3 + 2
+print "5-7?", 5 - 7
+print "greater", 5 >= -2
+print "less or equal", 5<= -2
