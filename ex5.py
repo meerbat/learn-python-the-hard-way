599c151f4e01734ceeaa5a1c315a8d8faf65f76b
miriam battistoni <mm.battistoni@gmail.com>
2015-11-23 17:20:00 +0100

    Ho fatto l'esercizio 5, yay!

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = "Zed Shaw"
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = "blue"
+my_teeth = "white"
+my_hair = "brown"
+
+print "Let's talk about %s ." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are %s depending on the coffee." % my_teeth
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+altezzacm = my_height *2.54
+print "He's %d cm high" % altezzacm
+
+pesokg = my_weight * 0.453592
+print "He's %d kg heavy" % pesokg
