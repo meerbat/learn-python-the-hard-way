d2282d61765efb95979f511adddd56818b4a49a5
miriam battistoni <mm.battistoni@gmail.com>
2015-11-24 10:59:03 +0100

    Printing: in cui imparo l'importanza dell'encoding dichiarato.

---
+++ esercizi/ex11.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+print "How old are you?",
+age = raw_input ()
+print "How tall are you?",
+height = raw_input ()
+print "How much do you weigh?",
+weight = raw_input ()
+
+print "So, you're %r old, %r tall, and %r heavy." % (age, height, weight)
+
+#study drills
+#scrivi un altro form simile
+
+print "Ciao! Come ti chiami?",
+nome = raw_input ()
+print u"Che giorno è oggi?"
+giorno = raw_input ()
+print "Di che colore sono le tue scarpe?"
+colore = raw_input ()
+
+print
+print "Ah, allora oggi è %r, e %s ha le scarpe %s!" % (giorno, nome, colore)
