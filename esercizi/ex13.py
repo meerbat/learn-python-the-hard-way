b7f138feb456a6b77b4a490d0e03c5220f5a6f4c
miriam battistoni <mm.battistoni@gmail.com>
2015-11-24 11:25:15 +0100

    13. In cui importa viariabili da fuori con argv.

---
+++ esercizi/ex13.py
@@ -0,0 +1,12 @@
+#Esercizio 13
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+##### quando deve essere eseguito, inserire ESATTAMENTE 3 var da tastiera dopo python ex13.py
