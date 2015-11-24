2a58f664e0b18e8932deeab58154940403352f1b
miriam battistoni <mm.battistoni@gmail.com>
2015-11-24 10:11:56 +0100

    Printing, in cui scopro le funzioni delle 3 virgolette.

---
+++ esercizi/ex9.py
@@ -0,0 +1,16 @@
+#Here's some strange new stuff, remember: type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+e ora un print lunghissimo
+lunghiiiisssimoooooooooooooo
+lungo quanto voglio
+riga
+riga
+riga
+"""
