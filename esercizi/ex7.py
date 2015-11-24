7be8012fd46fbf1cbdd37b3fe36ed1645711cd34
miriam battistoni <mm.battistoni@gmail.com>
2015-11-24 09:49:46 +0100

    Mary ha una pecorella + commenti

---
+++ esercizi/ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb."
+print "It's fleece was white as %s." % 'snow' #non è una variabile
+print "And everywhere that Mary went." #stampa in colonna
+print "." *10 # what'd that do? visualizza 10 puntini
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#attenta alla virgola alla fine, prova a toglierla e vedere che succede: concatena i print!
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
