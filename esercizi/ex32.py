32798821c4483911d908fd4e5adf16115a5ae1a8
miriam battistoni <mm.battistoni@gmail.com>
2015-11-25 16:34:17 +0100

    32: in cui conto liste scoordinate grazie al for.

---
+++ esercizi/ex32.py
@@ -0,0 +1,34 @@
+#Esercizio 32
+
+the_count = [1, 2 , 3, 4 , 5]
+fruits = ["apples", "oranges", "pears", "apricots"]
+change = [1, "pennies", 2, "dimes", 3, "quarters"]
+
+#primo tipo di loop che attraversa una lista.
+for number in the_count:
+    print "This is count %d." % number
+
+#same as above
+for fruit in fruits:
+    print "A fruit of type: %s." % fruit
+
+#attraverso liste mixate
+#usa %r per var se non sei sicuro del contenuto.
+for i in change:
+    print "I got %r" % i
+
+#we can also build lists, first start with an empty one.
+elements = []
+
+#then use the range function to do 0 to 5 counts
+for i in range(0,6):
+    print "Adding %d to the list." % i
+
+    #append is a function that lists understand
+    elements.append(i)
+
+#now we can print them out too
+for i in elements:
+    print "Element was: %d." % i
+
+###fineee###
