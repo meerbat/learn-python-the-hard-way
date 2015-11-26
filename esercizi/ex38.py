9e8f8d0ed97b87f36a4000f31baa8b7a6365f6a4
miriam battistoni <mm.battistoni@gmail.com>
2015-11-26 12:49:30 +0100

    38: in cui gioca con le liste.

---
+++ esercizi/ex38.py
@@ -0,0 +1,27 @@
+#Esercizio 38
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print "Wait there are not 100 things in that list. Let's fix that."
+
+stuff = ten_things.split(' ') 
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print "Adding: ", next_one
+    stuff.append(next_one)
+    print "There are %d items now." % len(stuff)
+
+print "There we go: ", stuff
+
+print "Let's do some things with stuff."
+
+print stuff[1]
+print stuff [-1] #penultimo!
+print stuff.pop()
+print ' '.join(stuff) #what?
+print '#'.join(stuff[3:5]) #super! #unisce 3 e 4: esclude 5!!!
+
+print sorted(stuff)
+
+###fineeee###
